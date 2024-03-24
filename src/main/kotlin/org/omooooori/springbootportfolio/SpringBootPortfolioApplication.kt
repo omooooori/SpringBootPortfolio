6656361ab0baf640030fa63b25e27da22f2afdd2
omooooori <omooooori432@gmail.com>
2024-03-24 12:49:58 +0900

    Initial commit.

---
+++ src/main/kotlin/org/omooooori/springbootportfolio/SpringBootPortfolioApplication.kt
@@ -0,0 +1,11 @@
+package org.omooooori.springbootportfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootPortfolioApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootPortfolioApplication>(*args)
+}
